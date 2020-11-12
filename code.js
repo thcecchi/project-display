5b2a4da8bdc805574d062fb6a59f03f93b4a87cc
Tyler Cecchi <thcecchi@gmail.com>
2020-11-12 00:30:38 -0500

    Initial commit

---
+++ code.js
@@ -0,0 +1,67 @@
+var Airtable = require('airtable');
+var base = new Airtable({apiKey: 'keyx4Y6k1k7YSNgiD'}).base('appN4cpWc7TV3YYd1');
+
+function getWord () {
+  base('Table 1').select({
+      view: 'Grid view'
+  }).firstPage(function(err, records) {
+      if (err) { console.error(err); return; }
+      // SINGLE
+      // const idx = records.length-1
+      // const word = records[idx].fields.Word
+      // $('#word').text(word);
+
+      // STACKED
+      const listLength = records.length
+      if (allWords.length == 0) {
+        for (i = allWords.length; i < records.length; i++) {
+          allWords.push(records[i].fields.Word);
+        }
+        showRandomMarquee()
+      } else if (records.length > allWords.length) {
+          for (i = allWords.length; i < records.length; i++) {
+            allWords.push(records[i].fields.Word);
+          }
+      }
+  });
+}
+
+var $mq = $('.marquee');
+
+function showRandomMarquee() {
+  var wordMarkup = [];
+  $mq
+    .marquee('destroy')
+    .html(function () {
+      allWords.forEach(function(idx){
+        var fontChoice = randomizeFont();
+        console.log(fontChoice)
+        wordMarkup.push('<li class="' + fontChoice + '">' + idx + '</li>')
+      })
+      var wordString = wordMarkup.join('');
+      return '<ul>' + wordString + '</ul>'
+    })
+  $mq
+    .marquee({duration: 9000, direction:'up'});
+
+  $mq.on('finished', showRandomMarquee);
+}
+
+function randomizeFont() {
+  var fonts = ['Gangster', 'PPWoodland', 'NeueMetana', 'NevradaNeue'];
+  var index = Math.floor(Math.random() * fonts.length);
+  var selectedFont = fonts[index]
+  return selectedFont;
+}
+
+
+  // Single
+  // getWord()
+
+  //STACKED
+  allWords = []
+  getWord()
+  //
+  setInterval(function(){
+    getWord()
+  }, 3000);
